8c3e8967f33f4a63b2eb9eb599aebc75d1cf1263
Kazuki3dayo <fate0007rx7@yahoo.co.jp>
2020-12-08 12:33:55 +0900

    ルーム作成・保存機能実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,9 +5,21 @@ class RoomsController < ApplicationController
   end
 
   def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+  
+  private
+
+  def room_params
+    params.require(:room).permit(:name, :password_digest, user_ids: []) #このパーミットの意味後で調べる
   end
 
 end
